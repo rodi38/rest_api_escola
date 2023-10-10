717d9cb24e31c840ebb2b11e1ae9b0d8908c736c
rodi38 <rodrigorochaed1@gmail.com>
2023-10-10 15:39:07 -0300

    adicionando crud usuarios

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,9 +4,89 @@ class UserController {
   async store(req, res) {
     try {
       const novoUser = await User.create(req.body);
-      res.json(novoUser);
+      return res.json(novoUser);
     } catch (e) {
-      res.status(400).json({
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          error: 'Usuario não encontrado',
+        });
+      }
+      const user = await User.findByPk(req.params.id);
+      if (!user) {
+        return res.status(404).json({
+          error: 'Usuario não encontrado',
+        });
+      }
+
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(404).json({
+          errors: ['id não enviado'],
+        });
+      }
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(404).json({
+          errors: ['Usuario não encontrado'],
+        });
+      }
+      const novoUser = await user.update(req.body);
+      return res.json(novoUser);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(404).json({
+          errors: ['id não enviado'],
+        });
+      }
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(404).json({
+          errors: ['Usuario não encontrado'],
+        });
+      }
+      user.destroy();
+      return res.json({
+        msg: 'Usuario deletado com sucesso',
+        user,
+      });
+    } catch (e) {
+      return res.status(400).json({
         errors: e.errors.map((err) => err.message),
       });
     }
