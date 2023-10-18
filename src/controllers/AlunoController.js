467f1e26d66202c3904a427e76bb5f60c2d3ffee
rodi38 <rodrigorochaed1@gmail.com>
2023-10-18 17:26:45 -0300

    aluno crud e rotas

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -9,6 +9,75 @@ class AlunoController {
       return res.json(null);
     }
   }
+
+  async store(req, res) {
+    try {
+      const aluno = await Aluno.create(req.body);
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(401).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async show(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.json('parametro não enviado');
+      }
+      const aluno = await Aluno.findByPk(req.params.id);
+      if (!aluno) {
+        return res.json('Aluno inexistente.');
+      }
+      return res.status(200).json(aluno);
+    } catch (e) {
+      return res.status(401).json({
+        errors: ['Aluno não encontrado'],
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      if (!req.params) {
+        return res.status(401).json('undefined params');
+      }
+      const aluno = await Aluno.findByPk(req.params.id);
+      if (!aluno) {
+        return res.status(401).json('aluno inexistente');
+      }
+      const updatedAluno = await aluno.update(req.body);
+      return res.json({
+        msg: 'Aluno atualizado com sucesso.',
+        updatedAluno,
+      });
+    } catch (e) {
+      return res.status(401).json({
+        errors: ['erro'],
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const aluno = await Aluno.findByPk(req.params.id);
+      if (!aluno) {
+        return res.status(401).json({
+          errors: ['Aluno não encontrado'],
+        });
+      }
+      await aluno.destroy();
+      return res.json({
+        msg: 'Aluno deletado com sucesso',
+        aluno,
+      });
+    } catch (e) {
+      return res.status(401).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
 }
 
 export default new AlunoController();
--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -4,5 +4,9 @@ import alunoController from '../controllers/AlunoController';
 const router = new Router();
 
 router.get('/', alunoController.index);
+router.get('/:id', alunoController.show);
+router.put('/:id', alunoController.update);
+router.post('/', alunoController.store);
+router.delete('/:id', alunoController.delete);
 
 export default router;
